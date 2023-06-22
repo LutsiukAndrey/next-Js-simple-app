6924a54f12cb83356ecb046591cac4c2313eeece
Andrii <lutsiukandrey1@gmail.com>
2023-06-22 11:29:13 +0300

    without css

--- next-app/app/blog/error.tsx
+++ next-app/app/blog/error.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 export default function ErrorWrapper({ error }: { error: Error }) {
-    return <h1>smt wrong {error.message }</h1>
+  return <h1>smt wrong {error.message}</h1>;
 }
